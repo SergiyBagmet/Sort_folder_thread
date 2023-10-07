bfdbc611ec8aeae1e9fc8e25ab477ee249db8151
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-10-07 21:33:24 +0300

    cls_MyLogger

--- my_logger.py
+++ my_logger.py
@@ -1,24 +1,32 @@
 import logging
 
-log_format = "[%(levelname)s] %(asctime)s: %(name)s %(module)s %(funcName)s:%(lineno)d - %(message)s"
-
-file_handler = logging.FileHandler("application.log")
-file_handler.setLevel(logging.ERROR)
-file_handler.setFormatter(logging.Formatter(log_format))
-
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.DEBUG)
-stream_handler.setFormatter(logging.Formatter(log_format))
-
-
-def get_logger(name, level):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
-
-    return logger
-
-
+class MyLogger:
+    def __init__(self, name: str='my_logger', log_level: int=logging.DEBUG, log_file: str | None=None):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(log_level)
+        self.log_format = logging.Formatter('[%(levelname)s] %(asctime)s: %(name)s %(module)s %(funcName)s:%(lineno)d - "%(message)s"')
+        # Создаем обработчик для вывода лога в консоль
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(self.log_format)
+        self.logger.addHandler(console_handler)
+        
+        # Если указан файл для логирования, создаем обработчик для записи в файл
+        if log_file:
+            self.add_file_handler(log_file)
+    
+    def set_log_level(self, log_level):
+        self.logger.setLevel(log_level)
+    
+    def add_file_handler(self, log_file):
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.ERROR)
+        file_handler.setFormatter(self.log_format)
+        self.logger.addHandler(file_handler)
+    
+    def get_logger(self):
+        return self.logger
+    
 if __name__ == "__main__":
-    pass
+    logger = MyLogger().get_logger()
+    logger.info("starting app")
+    
